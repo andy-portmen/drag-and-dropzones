4f0d9920262b8220270d2e681dbb81dde51858af
Andy Portmen <andy.portmen@gmail.com>
2016-11-21 16:54:27 +0330

    updating to the latest

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-zip -9 -r ddzones.xpi chrome defaults install.rdf chrome.manifest license.txt
+zip -9 -r ddzones.xpi chrome defaults install.rdf chrome.manifest license.txt -x "*.DS_Store"
 wget --post-file=ddzones.xpi http://localhost:8888/
